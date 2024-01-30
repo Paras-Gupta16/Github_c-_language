d4f5236c07eb70497c956a54f5a2c9d80403f01e
Paras-Gupta16 <paras94904@gmail.com>
2024-01-30 20:03:58 +0530

    Basic program in c
    how to calculate sum and difference in c

---
+++ paras1.c
@@ -0,0 +1,11 @@
+// Program to print sum and difference of two number 
+#include<stdio.h>
+int main()
+{   int i,j;
+    printf("enter two number:\n");
+    scanf("%d%d",&i,&j);
+    int sum=i+j;
+    int diff=i-j;
+    printf("sum=%d\n differance=%d\n",sum,diff);
+    return 0;
+}

2ae12bd60ad2ac2e47a5b26dc72ebd628903ec06
Paras-Gupta16 <paras94904@gmail.com>
2024-06-27 18:31:06 +0530

    Depth-First-Search(DFS)
    DFS can be implement using stack

---
+++ paras25.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int visited[4];
+int a[4][4]={
+    {1,0,1,0},
+    {0,0,1,1},
+    {1,1,1,0},
+    {1,1,1,1}
+};
+void dfs(int i){
+    int j;
+    printf("%d",i);
+    visited[i]=1;
+    for(j=0;j<4;j++){
+        if(a[i][j]==1 && !visited[j]){
+            dfs(j);
+        }
+    }
+}
+int main(){
+    dfs(1);
+    return 0;
+}
